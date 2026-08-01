f5d1ac5b825f00f77c8573aebf28ae6128fbed88
Max Hoogenbosch <mhoogenbosch@enrise.com>
2026-08-01 13:11:07 +0200

    Format blade templates

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,60 +1,49 @@
 @php
     /**
      * @bladestan-signature
+     *
      * @var array<string, array|string> $meta
      * @var string $size
      */
 @endphp
-<div class="flex-col justify-between gap-2 w-[300px] overflow-y-auto overflow-x-hidden hidden xl:flex">
-    <div class="flex flex-col p-4 divide-y divide-gray-600">
-        @foreach($meta as $label => $values)
+<div class="hidden w-[300px] flex-col justify-between gap-2 overflow-x-hidden overflow-y-auto xl:flex">
+    <div class="flex flex-col divide-y divide-gray-600 p-4">
+        @foreach ($meta as $label => $values)
             <div class="flex flex-col py-2">
-                <div class="text-xs font-bold tracking-wide uppercase">
-                    {{ $label }}
-                </div>
-                    @if(is_array($values))
-                    @foreach($values as $mail)
-                        <div class="text-sm truncate" title="{{ $mail }}">
-                            {{ $mail }}
-                        </div>
+                <div class="text-xs font-bold tracking-wide uppercase">{{ $label }}</div>
+                @if (is_array($values))
+                    @foreach ($values as $mail)
+                        <div class="truncate text-sm" title="{{ $mail }}">{{ $mail }}</div>
                     @endforeach
                 @else
-                    <div class="text-sm">
-                        {{ $values }}
-                    </div>
+                    <div class="text-sm">{{ $values }}</div>
                 @endif
             </div>
         @endforeach
-        @if(!empty($attachments))
-            <div class="flex flex-col py-2 gap-2">
-                <div class="text-xs font-bold tracking-wide uppercase">
-                    Attachments
-                </div>
-                <div class="flex flex-row flex-wrap gap-2 items-start">
-                    @foreach($attachments as $attachment)
-                        <div
-                            class="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-blue-500/50 text-primary-foreground hover:bg-primary/80">
+        @if (! empty($attachments))
+            <div class="flex flex-col gap-2 py-2">
+                <div class="text-xs font-bold tracking-wide uppercase">Attachments</div>
+                <div class="flex flex-row flex-wrap items-start gap-2">
+                    @foreach ($attachments as $attachment)
+                        <div class="focus:ring-ring text-primary-foreground hover:bg-primary/80 inline-flex items-center rounded-full border border-transparent bg-blue-500/50 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:ring-2 focus:ring-offset-2 focus:outline-hidden">
                             {{ $attachment }}
                         </div>
                     @endforeach
                 </div>
             </div>
         @endif
-        @if($size)
+        @if ($size)
             <div class="flex flex-col py-2">
-                <div class="text-xs font-bold tracking-wide uppercase">
-                    Size
-                </div>
-                <div class="text-sm">
-                    {{ $size }}
-                </div>
+                <div class="text-xs font-bold tracking-wide uppercase">Size</div>
+                <div class="text-sm">{{ $size }}</div>
             </div>
         @endif
     </div>
-    @if(config('mailbook.show_credits'))
-        <div class="text-gray-200 text-center text-xs p-2">
-            Created with <a href="https://github.com/Xammie/mailbook" target="_blank"
-                            class="text-white text-bold underline">mailbook</a>
+    @if (config('mailbook.show_credits'))
+        <div class="p-2 text-center text-xs text-gray-200">
+            Created with
+            <a href="https://github.com/Xammie/mailbook" target="_blank" class="text-bold text-white underline"
+                >mailbook</a>
         </div>
     @endif
 </div>
--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -1,21 +1,24 @@
 @php
     /**
      * @bladestan-signature
+     *
      * @var array|string|null $display
      * @var string $preview
      */
 @endphp
-<div class="flex flex-col flex-1 bg-gray-900">
-    <div class="text-black flex-1 flex justify-center bg-[#090816] relative">
-        <iframe @class([
-                    'w-full h-full bg-white',
-                    'max-w-md' => $display === 'phone',
-                    'max-w-3xl' => $display === 'tablet',
-                ])
-                sandbox="allow-scripts allow-same-origin allow-popups"
-                fetchpriority="high"
-                loading="eager"
-                src="{{ $preview }}"></iframe>
+<div class="flex flex-1 flex-col bg-gray-900">
+    <div class="relative flex flex-1 justify-center bg-[#090816] text-black">
+        <iframe
+            @class([
+                'w-full h-full bg-white',
+                'max-w-md' => $display === 'phone',
+                'max-w-3xl' => $display === 'tablet',
+            ])
+            sandbox="allow-scripts allow-same-origin allow-popups"
+            fetchpriority="high"
+            loading="eager"
+            src="{{ $preview }}"
+        ></iframe>
         @include('mailbook::breakpoints')
     </div>
 </div>
